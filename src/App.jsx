eccca7c5cad23b8a20aa46cd251d4a6f1d38a049
wjdrhkd456@thekeywe.com <wjdrhkd456@thekeywe.com>
2022-03-02 18:41:05 +0900

    [Ted] studyied

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+
+    setMovies(response.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>{movie.title}</div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default App;
